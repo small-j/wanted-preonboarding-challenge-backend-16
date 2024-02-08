4cd053717130305e24f3ffb687eb69fa08a43ede
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2024-02-08 13:46:26 +0900

    feat: PerformanceSeatInfo entity 추가

--- src/main/java/com/wanted/preonboarding/ticket/domain/entity/PerformanceSeatInfo.java
+++ src/main/java/com/wanted/preonboarding/ticket/domain/entity/PerformanceSeatInfo.java
@@ -1,4 +1,31 @@
 package com.wanted.preonboarding.ticket.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.sql.Date;
+
+@Getter
+@Builder
+@Entity
 public class PerformanceSeatInfo {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "performance_id")
+    private Performance performance;
+
+    @Column(nullable = false)
+    private int round;
+    @Column(nullable = false)
+    private int gate;
+    @Column(nullable = false)
+    private char line;
+    @Column(nullable = false)
+    private int seat;
+    @Column(nullable = false, columnDefinition = "varchar default 'disable'")
+    private String isReserve;
 }
