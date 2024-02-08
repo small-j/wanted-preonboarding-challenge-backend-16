95e181adb7d7b4c467eab133a49f9d8285c7e4fd
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2024-02-08 16:33:50 +0900

    feat: Performance save test 추가

--- src/test/java/com/wanted/preonboarding/ticket/domain/dto/PerformanceTests.java
+++ src/test/java/com/wanted/preonboarding/ticket/domain/dto/PerformanceTests.java
@@ -1,23 +1,54 @@
 package com.wanted.preonboarding.ticket.domain.dto;
 
+import com.wanted.preonboarding.ticket.domain.entity.Performance;
+import com.wanted.preonboarding.ticket.infrastructure.repository.PerformanceRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 
 @SpringBootTest
 public class PerformanceTests {
 
+    @Autowired
+    private PerformanceRepository performanceRepository;
+
     @Test
     public void enumTest() {
         int code = 2;
         String typeName = Arrays.stream(PerformanceType.values()).filter(value -> value.getCategory() == code)
-            .findFirst()
-            .orElse(PerformanceType.NONE)
-            .name();
+                .findFirst()
+                .orElse(PerformanceType.NONE)
+                .name();
         assertEquals("EXHIBITION", typeName);
+    }
+
+    @Test
+    public void performanceSave() {
+        // given
+        LocalDateTime now = LocalDateTime.now();
+        Performance performance = Performance.builder()
+                .name("레베카")
+                .price(100000)
+                .round(1)
+                .type(0)
+                .isReserve("enable")
+                .startDate(LocalDateTime.of(2024, 1, 14, 10, 34))
+                .build();
+
+        // when
+        Performance savedPerformance = performanceRepository.save(performance);
+
+        // then
+        Performance findPerformance = performanceRepository.findById(savedPerformance.getId())
+                .orElseThrow(EntityNotFoundException::new);
+        Assertions.assertThat(findPerformance.getId()).isEqualTo(savedPerformance.getId());
     }
 }
